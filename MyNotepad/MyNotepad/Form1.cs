053f37b3ed5a97f9ae2879b9b15f4578d3b52325
Rishabh Dhiman <51153454+RishabhDhiman99@users.noreply.github.com>
2020-01-18 19:40:02 +0530

    Add files via upload

---
+++ MyNotepad/MyNotepad/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNotepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+                richTextBox1.LoadFile(op.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = op.FileName;
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sv = new SaveFileDialog();
+            sv.Filter = "Text Document(*.txt)|*.txt|All Files(*.*)|*.*";
+            if (sv.ShowDialog() == DialogResult.OK)
+                richTextBox1.SaveFile(sv.FileName, RichTextBoxStreamType.PlainText);
+            this.Text = sv.FileName;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void UndoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void RedoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void FontToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.Font = richTextBox1.SelectionFont;
+            if(fd.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fd.Font;
+            }
+        }
+
+        private void BackgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog cr = new ColorDialog();
+            if(cr.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.BackColor = cr.Color;
+            }
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ver 1.0.0\nCreated by Rishabh Dhiman");
+        }
+    }
+}
